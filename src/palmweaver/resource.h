a3fdf9deb1fbbd47e26e23b01dcf3289b2dd1a84
ict <ict@nfinit.systems>
2026-01-30 23:41:21 -0700

    0.2.0.89: Removed ANSI PALMWEAVER_VERSION_A version number constant

--- src/palmweaver/resource.h
+++ src/palmweaver/resource.h
@@ -7,8 +7,7 @@
 #define RESOURCE_H
 
 /* Version */
-#define PALMWEAVER_VERSION      L"0.2.0.88"
-#define PALMWEAVER_VERSION_A    "0.2.0.88"
+#define PALMWEAVER_VERSION      L"0.2.0.89"
 
 /* Menu */
 #define IDR_MENU                101
